30e77b648c3c61f3ea8378343ec201e1ea5642fb
Sachin Yadav <121727034+sachinyadav1029@users.noreply.github.com>
2025-06-17 14:00:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 function fibonacci(num) {
-// your code here
+      if (num === 1) return 0;
+    if (num === 2) return 1;
+
+    let a = 0, b = 1;
+    for (let i = 3; i <= num; i++) {
+        [a, b] = [b, a + b];
+    }
+    return b;
+
 }
+
+function getFibonacci() {
+    const p = document.getElementById("result");
+    const inp = document.getElementById("input");
+
+    const num = parseInt(inp.value); // Convert string to number
+    if (!isNaN(num)) {
+        p.textContent = fibonacci(num);
+    } else {
+        p.textContent = "Please enter a valid number.";
+    }
+}
+
 
 module.exports = fibonacci;
